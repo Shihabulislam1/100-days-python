372133aabc54e31b222a81c456ab9a07288efd90
Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-09-16 20:00:11 +0600

    day 1 comipleted

--- Day 1/learn.py
+++ Day 1/learn.py
@@ -9,17 +9,23 @@
 # print(len(var))
 # print(var)
 
-a=input("a: ")
-b=input("b: ")
-print("a: "+a)
-print("b: "+b)
-a=int(a)
-b=int(b)
-a=a+b
-b=a-b
-a=a-b
+# a=input("a: ")
+# b=input("b: ")
+# print("a: "+a)
+# print("b: "+b)
+# a=int(a)
+# b=int(b)
+# a=a+b
+# b=a-b
+# a=a-b
 
-print("a= ",a)
-print("b= ",b)
+# print("a= ",a)
+# print("b= ",b)
 
 
+
+# print("Hello " + input("what is your name?"))
+
+name=input("What is your name? ")
+
+print("Your name has "+str(len(name))+" characters")
---
+++ Day 1/project_day_1.py
@@ -0,0 +1,11 @@
+
+
+print("Welcome to the Band Name Generator.")
+
+city =input("What's the name of the city you grew up in?\n")
+
+pet=input ("What's the  name of your pet?\n")
+
+band_name="'"+city+" "+pet+"'"
+
+print("your Band Name could be "+band_name)
