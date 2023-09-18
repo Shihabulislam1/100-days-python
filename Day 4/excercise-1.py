66849f3b02ce65f9c17fb2afdfc1b52a82f97009
Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-09-18 07:41:22 +0600

    day 4 progress

---
+++ Day 4/excercise-1.py
@@ -0,0 +1,9 @@
+
+import random as rd
+
+num=rd.randint(0,1)
+
+if num==0:
+    print("Heads")
+elif num==1:
+    print("Tails")
--- Day 4/randomisation-lists.py
+++ Day 4/randomisation-lists.py
@@ -3,6 +3,8 @@ import random as rd
 import mymodule
 
 rand_int=rd.randint(1,11)
+rand_float=rd.random()
+rand_float2=rd.random()*5
 
-print(rand_int)
+print(rand_int, rand_float,rand_float2)
 print(mymodule.pi)
