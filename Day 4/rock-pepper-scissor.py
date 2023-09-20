7e0e5c35cc79032971272093a414edb76a899e25
Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-09-20 14:02:24 +0600

    day 4 rock peppper scissor

--- Day 4/rock-pepper-scissor.py
+++ Day 4/rock-pepper-scissor.py
@@ -29,10 +29,26 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 
-user_c=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print("You chose:")
-print(game_images[user_c])
+u=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-com_c=rd.randint(0,2)
-print("Computer chose:")
-print(game_images[com_c])
+if u>=3 or u<0:
+  print("You typed an invalid number, you lose!")
+else:
+    print("You chose:")
+    print(game_images[u])
+
+    c=rd.randint(0,2)
+    print("Computer chose:")
+    print(game_images[c])
+
+    if u==c:
+       print("It's a draw")
+    elif u==0 and c==2:
+       print("You win")
+    elif u==2 and c==0:
+         print("You lose")
+    elif u>c:
+            print("You win")
+    elif c>u:
+            print("You lose")
+    
